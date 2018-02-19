1e44dc44597c96b8d0fdf681cf06ed7be56b3610
actribiana <36612265+actribiana@users.noreply.github.com>
2018-02-19 05:20:19 +0000

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,8 @@
+for num in range (1,6):
+    print (num, "squared is", num * num)
+print " "
+for num in range (1,6,2):
+    print (num, "squared is", num * num)
+print " "
+for num in range (10,0,-1):
+    print (num, "squared is", num * num)
---
+++ while loop.py
@@ -0,0 +1,4 @@
+num = 1
+while num < 200:
+    print num
+    num = num * 2
